f81e0a8356cf879aecf7fb99bf817713a4ca28e8
Bruno Dutra <brunocodutra@gmail.com>
2016-12-12 21:26:31 +0100

    setup versioning

--- include/alloy/config.hpp
+++ include/alloy/config.hpp
@@ -2,9 +2,9 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt
 
-#ifndef ALLOY_HPP
-#define ALLOY_HPP
+#ifndef ALLOY_CONFIG_HPP
+#define ALLOY_CONFIG_HPP
 
-#include "alloy/config.hpp"
+#include "config/version.hpp"
 
 #endif
