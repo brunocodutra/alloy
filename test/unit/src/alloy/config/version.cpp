cd88d90f0d8ec22cc38a832ab42d103b56282bb6
Bruno Dutra <brunocodutra@gmail.com>
2016-12-19 20:26:21 +0100

    add a unit test for the versioning

---
+++ test/unit/src/alloy/config/version.cpp
@@ -0,0 +1,31 @@
+// Copyright Bruno Dutra 2016
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt
+
+#include <alloy.hpp>
+
+#if EXPECTED_ALLOY_MAJOR != ALLOY_MAJOR
+#error major version mismatch!
+#endif
+
+#if EXPECTED_ALLOY_MINOR != ALLOY_MINOR
+#error minor version mismatch!
+#endif
+
+#if EXPECTED_ALLOY_PATCH != ALLOY_PATCH
+#error patch version mismatch!
+#endif
+
+#define EXPECTED_ALLOY_VERSION ALLOY_SEMVER( \
+    EXPECTED_ALLOY_MAJOR, \
+    EXPECTED_ALLOY_MINOR, \
+    EXPECTED_ALLOY_PATCH \
+)
+
+#if EXPECTED_ALLOY_VERSION != ALLOY_VERSION
+#error semantic version mismatch!
+#endif
+
+int main() {
+    return 0;
+}
