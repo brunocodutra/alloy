e7939f79a317dd3ed091919e4783e7891a0b8602
Bruno Dutra <brunocodutra@gmail.com>
2017-02-06 22:57:57 +0100

    provide alloy::invoke as a constexpr implementation of std::invoke

--- include/alloy/lambda.hpp
+++ include/alloy/lambda.hpp
@@ -2,12 +2,9 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt
 
-#ifndef ALLOY_HPP
-#define ALLOY_HPP
+#ifndef ALLOY_LAMBDA_HPP
+#define ALLOY_LAMBDA_HPP
 
-#include <alloy/config.hpp>
-#include <alloy/lambda.hpp>
-#include <alloy/number.hpp>
-#include <alloy/tuple.hpp>
+#include <alloy/lambda/invoke.hpp>
 
 #endif
---
+++ include/alloy/lambda/invoke.hpp
@@ -0,0 +1,63 @@
+// Copyright Bruno Dutra 2016-2017
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt
+
+#ifndef ALLOY_LAMBDA_INVOKE_HPP
+#define ALLOY_LAMBDA_INVOKE_HPP
+
+#include <alloy/config.hpp>
+
+#include <alloy/detail/return.hpp>
+#include <alloy/detail/traits.hpp>
+
+namespace alloy::detail {
+    template<typename Base, typename F, typename Ref, typename... Args,
+        where<std::is_function<F>::value> = valid,
+        where<std::is_base_of<Base, strip<Ref>>::value> = valid
+    >
+    constexpr auto invoke(F Base::* pmf, Ref&& ref, Args&&... args) ALLOY_RETURN(
+        (static_cast<Ref&&>(ref).*pmf)(static_cast<Args&&>(args)...)
+    )
+
+    template <typename Base, typename F, typename Ptr, typename... Args,
+        where<std::is_function<F>::value> = valid,
+        where<!std::is_base_of<Base, strip<Ptr>>::value> = valid
+    >
+    constexpr auto invoke(F Base::* pmf, Ptr&& ptr, Args&&... args) ALLOY_RETURN(
+        ((*static_cast<Ptr&&>(ptr)).*pmf)(static_cast<Args&&>(args)...)
+    )
+
+    template <typename Base, typename D, typename Ref,
+        where<!std::is_function<D>::value> = valid,
+        where<std::is_base_of<Base, strip<Ref>>::value> = valid
+    >
+    constexpr auto invoke(D Base::* pmd, Ref&& ref) ALLOY_RETURN(
+        static_cast<Ref&&>(ref).*pmd
+    )
+
+    template <typename Base, typename D, typename Ptr,
+        where<!std::is_function<D>::value> = valid,
+        where<!std::is_base_of<Base, strip<Ptr>>::value> = valid
+    >
+    constexpr auto invoke(D Base::* pmd, Ptr&& ptr) ALLOY_RETURN(
+        (*static_cast<Ptr&&>(ptr)).*pmd
+    )
+
+    template <typename Lbd, typename... Args>
+    constexpr auto invoke(Lbd&& lbd, Args&&... args) ALLOY_RETURN(
+        static_cast<Lbd&&>(lbd)(static_cast<Args&&>(args)...)
+    )
+
+    inline constexpr struct {
+        template<typename... Args>
+        constexpr auto operator ()(Args&&... args) ALLOY_RETURN(
+            invoke(static_cast<Args&&>(args)...)
+        )
+    } _invoke{};
+}
+
+namespace alloy {
+    inline constexpr auto& invoke = detail::_invoke;
+}
+
+#endif
