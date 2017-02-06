81eccc687fe2b3d7ff5ff7547f3ee322da272287
Bruno Dutra <brunocodutra@gmail.com>
2017-02-06 21:08:09 +0100

    bump copyright notice

--- include/alloy.hpp
+++ include/alloy.hpp
@@ -1,4 +1,4 @@
-// Copyright Bruno Dutra 2016
+// Copyright Bruno Dutra 2016-2017
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt
 
