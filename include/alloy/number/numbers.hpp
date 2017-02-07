c70f4af30e90f6a7a723d5a767f3dd050b3b30f3
Bruno Dutra <brunocodutra@gmail.com>
2017-02-07 22:55:14 +0100

    provide alloy::numbers as an alias to a alloy::tuple of alloy::number[s]

--- include/alloy/number/numbers.hpp
+++ include/alloy/number/numbers.hpp
@@ -2,13 +2,16 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt
 
-#ifndef ALLOY_NUMBER_HPP
-#define ALLOY_NUMBER_HPP
+#ifndef ALLOY_NUMBER_NUMBERS_HPP
+#define ALLOY_NUMBER_NUMBERS_HPP
 
-#include <alloy/number/and.hpp>
-#include <alloy/number/not.hpp>
+#include <alloy/config.hpp>
 #include <alloy/number/number.hpp>
-#include <alloy/number/numbers.hpp>
-#include <alloy/number/or.hpp>
+#include <alloy/tuple/tuple.hpp>
+
+namespace alloy {
+    template<auto... Is>
+    using numbers = tuple<number<Is>...>;
+}
 
 #endif
