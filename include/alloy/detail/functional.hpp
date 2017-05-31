70a02a20b6ca43dd1759ffd68081638778983e46
Bruno Dutra <brunocodutra@gmail.com>
2017-05-31 19:55:58 +0200

    implement detail::foldl and detail::foldr as generalized helpers
    for fold expressions

--- include/alloy/detail/functional.hpp
+++ include/alloy/detail/functional.hpp
@@ -51,6 +51,56 @@ namespace alloy::detail {
     constexpr auto invoke(F&& f, Args&&... args) ALLOY_RETURN(
         static_cast<F&&>(f)(static_cast<Args&&>(args)...)
     )
+
+    struct nil {};
+
+    template<typename F, typename Arg>
+    struct folder {
+        F&& f;
+        Arg&& arg;
+
+        constexpr F&& operator +() noexcept {
+            return static_cast<F&&>(f);
+        }
+
+        constexpr Arg&& operator *() noexcept {
+            return static_cast<Arg&&>(arg);
+        }
+
+        friend constexpr decltype(auto) operator >>=(nil, folder ref) noexcept {
+            return *ref;
+        }
+
+        template<typename State>
+        friend constexpr decltype(auto) operator >>=(State&& state, folder ref) {
+            return invoke(+ref, static_cast<State&&>(state), *ref);
+        }
+
+        friend constexpr decltype(auto) operator <<=(folder ref, nil) noexcept {
+            return *ref;
+        }
+
+        template<typename State>
+        friend constexpr decltype(auto) operator <<=(folder ref, State&& state) {
+            return invoke(+ref, *ref, static_cast<State&&>(state));
+        }
+    };
+
+    template<typename F, typename... Args>
+    constexpr decltype(auto) foldl(F&& f, Args&&... args) {
+        return (nil{} >>= ... >>= folder<F, Args>{
+            static_cast<F&&>(f),
+            static_cast<Args&&>(args)
+        });
+    }
+
+    template<typename F, typename... Args>
+    constexpr decltype(auto) foldr(F&& f, Args&&... args) {
+        return (folder<F, Args>{
+            static_cast<F&&>(f),
+            static_cast<Args&&>(args)
+        } <<= ... <<= nil{});
+    }
 }
 
 #undef ALLOY_RETURN
