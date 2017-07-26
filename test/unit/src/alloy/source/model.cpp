ac3bb7b88788e5c6ed5fa0eb2f85e895f4f2d531
Bruno Dutra <brunocodutra@gmail.com>
2017-07-26 23:11:28 +0200

    add unit tests for alloy::source

---
+++ test/unit/src/alloy/source/model.cpp
@@ -0,0 +1,22 @@
+// Copyright Bruno Dutra 2017
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt
+
+#include <alloy.hpp>
+
+#include "test.hpp"
+
+template<auto X, auto Y, auto Z>
+struct matrix {
+    constexpr matrix() {
+        auto f = [](auto&& sink) {
+            return values<X, Y, Z>()(FWD(sink));
+        };
+
+        static_assert(qualify<X>(alloy::source{callable<X>(f)}) >> expect(values<X, Y, Z>()));
+    }
+};
+
+int main() {
+    return test<matrix>;
+}
